06958f8c635d6e7037bf5ebfe64f734511c33f49
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-03 00:15:18 -0400

    First Module

---
+++ 03-node_tutorial/tutorial/04-names.js
@@ -0,0 +1,10 @@
+// local
+const secrete = 'SUPER SECRET';
+// share
+const adrian = 'adrian';
+const peter = 'peter';
+
+// console.log(module);
+
+// Exports (CommonJS)
+module.exports = { adrian, peter };
--- 03-node_tutorial/tutorial/app.js
+++ 03-node_tutorial/tutorial/app.js
@@ -1,11 +1,12 @@
-// Modules
-const adrian = 'adrian';
-const peter = 'peter';
-
-const sayHi = (name) => {
-  console.log(`Hello there ${name}`);
-};
+// CommonJS, every ile is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const names = require('./04-names');
+// const { adrian, peter } = require('./04-names');
+const sayHi = require('./05-utils');
+// console.log(names);
 
 sayHi('susan');
-sayHi(adrian);
-sayHi(peter);
+// sayHi(adrian);
+sayHi(names.adrian);
+// sayHi(peter);
+sayHi(names.peter);
