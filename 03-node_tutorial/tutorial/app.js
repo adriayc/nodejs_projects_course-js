dfa37213281dba152ab6db286cc80f1a113f723e
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-03 00:49:42 -0400

    NPM Command

---
+++ 03-node_tutorial/tutorial/app.js
@@ -0,0 +1,10 @@
+// npm - global command, comes with node
+// npm --version
+// npm --v
+
+// Local dependency - use it only in this particular project
+// npm i <packageName>
+
+// Global dependency - use it in any project
+// npm install -g <packageName>
+// sudo install -b <packageName> (Mac)
