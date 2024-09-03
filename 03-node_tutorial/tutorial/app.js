7abfa391d8ca69b1cf7a5d47cc434db6693d46bd
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-03 01:37:08 -0400

    Events Emitter - Additional Info

--- 03-node_tutorial/tutorial/app.js
+++ 03-node_tutorial/tutorial/app.js
@@ -3,9 +3,16 @@ const EventEmitter = require('events');
 const customEmitter = new EventEmitter();
 
 // on - listen for an event
+// customEmitter.on('response', () => {
+//   console.log(`Data received`);
+// });
+customEmitter.on('response', (name, id) => {
+  console.log(`Data received user ${name} with id: ${id}`);
+});
 customEmitter.on('response', () => {
-  console.log(`Data received`);
+  console.log(`Some other logic here`);
 });
 
 // emit - emit an event
-customEmitter.emit('response');
+// customEmitter.emit('response');
+customEmitter.emit('response', 'Adrian', 34);
