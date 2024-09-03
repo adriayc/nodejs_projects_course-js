6590db6ba2a8d87b88f5bf9e660e80da17089749
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-03 00:23:28 -0400

    Mind Grenade

--- 03-node_tutorial/tutorial/app.js
+++ 03-node_tutorial/tutorial/app.js
@@ -4,11 +4,13 @@ const names = require('./04-names');
 // const { adrian, peter } = require('./04-names');
 const sayHi = require('./05-utils');
 const data = require('./06-alternative-flavor');
+// Self invoke
+require('./07-mind-grenade');
 // console.log(names);
-console.log(data);
+// console.log(data);
 
-sayHi('susan');
+// sayHi('susan');
 // sayHi(adrian);
-sayHi(names.adrian);
+// sayHi(names.adrian);
 // sayHi(peter);
-sayHi(names.peter);
+// sayHi(names.peter);
