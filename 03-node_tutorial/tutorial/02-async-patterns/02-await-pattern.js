e2b4ad7af9ea0430e14f45973fb26ef9964e4eb9
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-03 01:19:44 -0400

    Async Patterns - Setup Promises

--- 03-node_tutorial/tutorial/02-async-patterns/02-await-pattern.js
+++ 03-node_tutorial/tutorial/02-async-patterns/02-await-pattern.js
@@ -1,13 +1,5 @@
 const { readFile } = require('fs');
 
-// readFile('./content/first.txt', 'utf8', (err, data) => {
-//   if (err) {
-//     return;
-//   } else {
-//     console.log(data);
-//   }
-// });
-
 const getText = (path) => {
   // Creating a Promise
   return new Promise((resolve, reject) => {
