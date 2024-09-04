d001452f352f772daa8bee8b7bbb302688fb9443
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-04 01:12:30 -0400

    HTTP - Basics

--- 04-express-tutorial/tutorial/app.js
+++ 04-express-tutorial/tutorial/app.js
@@ -1,1 +1,13 @@
-console.log('Express Tutorial');
+const http = require('http');
+
+// Create server
+const server = http.createServer((req, res) => {
+  console.log('User hit the server');
+  res.end('Home page');
+});
+
+// Listening port
+server.listen(5000);
+
+// TEST: Browser | Thunder Client
+// GET: http://localhost:5000
