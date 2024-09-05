cbd4a97cb7b7c44099046a0ff9292b04c6a9dd89
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-05 11:04:37 -0400

    HTTP - Request Object

--- 04-express-tutorial/tutorial/01-http-basics.js
+++ 04-express-tutorial/tutorial/01-http-basics.js
@@ -1,18 +1,13 @@
 const http = require('http');
 
-// Create server
 const server = http.createServer((req, res) => {
-  // Request
-  // console.log(req);
   // console.log(req.method);
-  // console.log(req.url);
 
   const url = req.url;
   // Home page
   if (url === '/') {
     // Headers
     res.writeHead(200, { 'content-type': 'text/html' }); // 200 OK
-    // res.writeHead(404, { 'content-type': 'text/html' }); // 404 Not Found
 
     res.write('<h1>Home page</h1>');
     res.end();
@@ -33,8 +28,4 @@ const server = http.createServer((req, res) => {
   }
 });
 
-// Listening port
 server.listen(5000);
-
-// TEST: Browser | Thunder Client
-// GET: http://localhost:5000
