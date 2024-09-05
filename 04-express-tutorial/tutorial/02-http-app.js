df45b80f52216660deea02281b247b987cbea10c
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-05 12:07:05 -0400

    HTTP - App Example

--- 04-express-tutorial/tutorial/02-http-app.js
+++ 04-express-tutorial/tutorial/02-http-app.js
@@ -2,28 +2,19 @@ const http = require('http');
 const { readFileSync } = require('fs');
 
 // Get all files
-// const homePage = readFileSync('./index.html');
 const homePage = readFileSync('./navbar-app/index.html');
 const homeStyles = readFileSync('./navbar-app/styles.css');
 const homeImage = readFileSync('./navbar-app/logo.svg');
 const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
-// Create server
 const server = http.createServer((req, res) => {
-  // Request
-  // console.log(req);
   // console.log(req.method);
-  // console.log(req.url);
 
   const url = req.url;
   // Home page
   if (url === '/') {
-    // Headers
     res.writeHead(200, { 'content-type': 'text/html' }); // 200 OK
-    // res.writeHead(200, { 'content-type': 'text/plain' }); // 200 OK
-    // res.writeHead(404, { 'content-type': 'text/html' }); // 404 Not Found
 
-    // res.write('<h1>Home page</h1>');
     res.write(homePage);
     res.end();
   }
@@ -48,6 +39,7 @@ const server = http.createServer((req, res) => {
     res.write(homeLogic);
     res.end();
   }
+
   // About page
   else if (url === '/about') {
     res.writeHead(200, { 'content-type': 'text/html' });
@@ -64,8 +56,4 @@ const server = http.createServer((req, res) => {
   }
 });
 
-// Listening port
 server.listen(5000);
-
-// TEST: Browser | Thunder Client
-// GET: http://localhost:5000
