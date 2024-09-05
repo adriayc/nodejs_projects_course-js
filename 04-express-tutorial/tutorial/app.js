b05611f3da0af945a9f120d4b6f42b1f849332c4
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-05 01:16:09 -0400

    HTTP Headers

--- 04-express-tutorial/tutorial/app.js
+++ 04-express-tutorial/tutorial/app.js
@@ -2,8 +2,16 @@ const http = require('http');
 
 // Create server
 const server = http.createServer((req, res) => {
-  console.log('User hit the server');
-  res.end('Home page');
+  // console.log('User hit the server');
+  // res.end('Home page');
+
+  // Headers
+  res.writeHead(200, { 'content-type': 'text/html' }); // 200 OK
+  // res.writeHead(404, { 'content-type': 'text/html' }); // 404 Not Found
+  // res.writeHead(200, { 'content-type': 'text/plain' });
+
+  res.write('<h1>Home page</h1>');
+  res.end();
 });
 
 // Listening port
