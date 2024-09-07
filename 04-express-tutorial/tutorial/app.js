23ee7780e1505509aaf12d9705093a6a8964599c
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-07 01:09:52 -0400

    Query String

--- 04-express-tutorial/tutorial/app.js
+++ 04-express-tutorial/tutorial/app.js
@@ -43,6 +43,31 @@ app.get('/api/products/:productId/reviews/:reviewId', (req, res) => {
   res.send('Hello world');
 });
 
+// Query string
+// URL: http://localhost:5000/api/v1/query?search=a&limit=2
+app.get('/api/v1/query', (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  if (sortedProducts.length < 1) {
+    // res.status(200).send('No products matched your search');
+    res.status(200).json({ success: true, data: [] });
+  }
+
+  // res.send('Hello world');
+  res.status(200).json(sortedProducts);
+});
+
 app.listen(5000, () => {
   console.log('Server is running on port 5000...');
 });
