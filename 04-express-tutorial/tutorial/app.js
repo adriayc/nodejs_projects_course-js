ee2083772e2e70dbe0c0037475e721cd5f0c124c
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-07 23:58:36 -0400

    Methods - POST (Form Example)

--- 04-express-tutorial/tutorial/app.js
+++ 04-express-tutorial/tutorial/app.js
@@ -5,10 +5,23 @@ const app = express();
 
 // Static assets
 app.use(express.static('./methods-public'));
+// Parse form data (Middleware to parse URL-encoded data)
+app.use(express.urlencoded({ extended: false }));
 
 // GET
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+// POST
+app.post('/login', (req, res) => {
+  // console.log(req.body);
+  const { name } = req.body;
+
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send('Please provider credentials');
 });
 
 app.listen(5000, () => {
