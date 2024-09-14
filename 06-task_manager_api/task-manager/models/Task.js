7a55e8349bde568b76f83392b4beeb666bef2231
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-14 18:50:52 -0400

    Basic Validation

--- 06-task_manager_api/task-manager/models/Task.js
+++ 06-task_manager_api/task-manager/models/Task.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose');
 
 // Create Task Schema
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  // Property validations
+  name: {
+    type: String,
+    trim: true,
+    maxLength: [20, 'Name can not be more than 20 characters'],
+    required: [true, 'Must provide name'],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 // Exports and create model
