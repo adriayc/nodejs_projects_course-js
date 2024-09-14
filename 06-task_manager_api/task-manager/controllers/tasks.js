ae2a2aa06c3c528b9b9d59223431474670af3272
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-14 19:44:30 -0400

    getSingleTask

--- 06-task_manager_api/task-manager/controllers/tasks.js
+++ 06-task_manager_api/task-manager/controllers/tasks.js
@@ -21,9 +21,21 @@ const createTask = async (req, res) => {
     res.status(500).json({ msg: error });
   }
 };
-const getTask = (req, res) => {
-  //   res.send('Get single task');
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    // const task = await Task.findById(taskID);
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    // console.log(error);
+    res.status(500).json({ msg: error });
+  }
 };
 const updateTask = (req, res) => {
   res.send('Update task');
