a34e5a1940c5c4958b6e91c50be059fd94ec9d16
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-15 00:33:35 -0400

    Response Types

--- 06-task_manager_api/task-manager/controllers/tasks.js
+++ 06-task_manager_api/task-manager/controllers/tasks.js
@@ -5,7 +5,14 @@ const Task = require('../models/Task');
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
-    res.status(200).json({ tasks });
+
+    // res.status(200).json({ tasks });
+    // Other types of response
+    // res.status(200).json({ tasks, amount: tasks.length });
+    res
+      .status(200)
+      // .json({ success: true, data: { tasks, nbHits: tasks.length } });
+      .json({ status: 'success', data: { tasks, nbHits: tasks.length } });
   } catch (error) {
     // console.log(error);
     res.status(500).json({ msg: error });
