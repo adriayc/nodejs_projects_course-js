84f03e3d76aad892e7ab1341a45de425a16176f6
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-15 23:20:04 -0400

    Basic Express App

--- 07-store_api/store-api/app.js
+++ 07-store_api/store-api/app.js
@@ -1,1 +1,35 @@
-console.log('Store API');
+require('dotenv').config();
+const express = require('express');
+// Middlewares
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products route</a>');
+});
+
+// Product route
+
+// Not found and Error handler (Middlewares)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = () => {
+  try {
+    // ConnectDB
+
+    app.listen(port, console.log(`Server is listening port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
