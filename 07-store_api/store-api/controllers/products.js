82c7f4d3f375b55168ea5448015565d1026b87e6
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-19 00:50:26 -0400

    Name

--- 07-store_api/store-api/controllers/products.js
+++ 07-store_api/store-api/controllers/products.js
@@ -1,14 +1,16 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
+  const search = 'ab';
   const products = await Product.find({
-    page: 2,
+    // name: 'albany ',
+    name: { $regex: search, $options: 'i' },
   });
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -16,6 +18,9 @@ const getAllProducts = async (req, res) => {
   }
   if (company) {
     queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
   }
   console.log(queryObject);
 
