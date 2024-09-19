01b9c91ce7891516b66841ace25590a9c3292f77
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-19 17:52:05 -0400

    Sort - getAllProducts Implementation

--- 07-store_api/store-api/controllers/products.js
+++ 07-store_api/store-api/controllers/products.js
@@ -9,7 +9,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -21,9 +21,18 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
   }
-  console.log(queryObject);
+  // console.log(queryObject);
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  // Sort
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    // console.log(sortList);
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createAt');
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
