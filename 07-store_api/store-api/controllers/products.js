48767cd19dbf89c667e4f6b1180829e05a406999
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-19 18:29:16 -0400

    Pagination

--- 07-store_api/store-api/controllers/products.js
+++ 07-store_api/store-api/controllers/products.js
@@ -10,6 +10,7 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, nbHits: products.length });
 };
 
+// URL: http://localhost:3000/api/v1/products?limit=5&page=1
 const getAllProducts = async (req, res) => {
   const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
@@ -40,6 +41,13 @@ const getAllProducts = async (req, res) => {
     filedList = fields.split(',').join(' ');
     result = result.select(filedList);
   }
+
+  // Pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
