fe82b0f57fd5d747fff0d8eb8b708c965ed2a927
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-19 18:05:35 -0400

    Skip and Limit - General Info

--- 07-store_api/store-api/controllers/products.js
+++ 07-store_api/store-api/controllers/products.js
@@ -1,8 +1,12 @@
 const Product = require('../models/product');
 
-// URL: http://localhost:3000/api/v1/products/static?fields=name,price
+// URL: http://localhost:3000/api/v1/products/static?sort=name&fields=name,price
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price');
+  const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(10) // limit - Specifies the maximum number of docs the query will return
+    .skip(5); // skip - Specifies the number of docs to skip
   res.status(200).json({ products, nbHits: products.length });
 };
 
