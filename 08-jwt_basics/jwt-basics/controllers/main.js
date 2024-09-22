a09d7b713246bca5ec6eb9b83d71e142096e5045
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-22 13:10:20 -0400

    Validate Username and Password

--- 08-jwt_basics/jwt-basics/controllers/main.js
+++ 08-jwt_basics/jwt-basics/controllers/main.js
@@ -1,4 +1,22 @@
+const CustomAPIError = require('../errors/custom-error');
+
+// Check username, password in post(login) request
+// If exists create new JWT
+// Send back to front-end
+
+// Setup authentication so only the request with JWT can access the dashboard
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  // Mongo validation
+  // Joi
+  // Check in the controller
+
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+
   res.send('Fake Login/Register/Signup Router');
 };
 
