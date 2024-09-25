77329575f634d57d250e8ec56d66eaf47ca3b99b
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-25 16:31:40 -0400

    Create User - Basic Setup

--- 09-jobs_api/jobs-api/controllers/auth.js
+++ 09-jobs_api/jobs-api/controllers/auth.js
@@ -1,5 +1,11 @@
+const { StatusCodes } = require('http-status-codes');
+// Models
+const User = require('../models/User');
+
 const register = async (req, res) => {
-  res.send('Register user');
+  const user = await User.create({ ...req.body });
+
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
