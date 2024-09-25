5b72a1daf1a92f689eaf3d0d994ac66169e2fd8f
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-25 16:32:14 -0400

    Error Checking - Controller Example

--- 09-jobs_api/jobs-api/controllers/auth.js
+++ 09-jobs_api/jobs-api/controllers/auth.js
@@ -1,10 +1,15 @@
 const { StatusCodes } = require('http-status-codes');
 // Models
 const User = require('../models/User');
+// Errors
+const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
+  // const { name, email, password } = req.body;
+  // if (!name || !email || !password) {
+  //   throw new BadRequestError('Please provide name, email and password');
+  // }
   const user = await User.create({ ...req.body });
-
   res.status(StatusCodes.CREATED).json({ user });
 };
 
