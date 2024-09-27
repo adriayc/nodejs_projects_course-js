2fdf0a68d91d6913cdb3503a2aa820899996ae46
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-27 17:32:17 -0400

    Validation Error

--- 09-jobs_api/jobs-api/middleware/error-handler.js
+++ 09-jobs_api/jobs-api/middleware/error-handler.js
@@ -10,6 +10,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // if (err instanceof CustomAPIError) {
   //   return res.status(err.statusCode).json({ msg: err.message });
   // }
+  if (err.name === 'ValidationError') {
+    // console.log(Object.values(err.errors));
+    customError.statusCode = 400;
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+  }
   if (err.code && err.code === 11000) {
     customError.statusCode = 400;
     customError.msg = `Duplicate value entered for ${Object.keys(
