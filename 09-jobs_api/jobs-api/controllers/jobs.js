6986b04bc916038c8862c749b4687e49ef552151
Adriano Ayala <adriano.ayc@gmail.com>
2024-09-27 16:16:14 -0400

    Remove Job

--- 09-jobs_api/jobs-api/controllers/jobs.js
+++ 09-jobs_api/jobs-api/controllers/jobs.js
@@ -53,7 +53,17 @@ const updateJob = async (req, res) => {
   res.status(StatusCodes.OK).json({ job });
 };
 const deleteJob = async (req, res) => {
-  res.send('Delete job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findByIdAndDelete({ _id: jobId, createdBy: userId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
