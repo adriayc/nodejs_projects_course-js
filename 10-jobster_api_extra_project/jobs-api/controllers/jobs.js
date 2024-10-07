bc25cbc5565f5799db8dccbea36d2b58e0ade00c
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-07 18:28:14 -0400

    Pagination

--- 10-jobster_api_extra_project/jobs-api/controllers/jobs.js
+++ 10-jobster_api_extra_project/jobs-api/controllers/jobs.js
@@ -41,8 +41,20 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position');
   }
 
+  // Pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res.status(StatusCodes.OK).json({ jobs });
+
+  // Total jobs and Number of pages
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit); // The Math.ceil() function in JS is used to round a number up to the nearest integer.
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 const getJob = async (req, res) => {
   const {
