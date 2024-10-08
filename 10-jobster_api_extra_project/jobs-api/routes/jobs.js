c7cf523ada5fc7b7a54d64051759976e02f1c7b3
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-08 00:49:29 -0400

    ShowStats Controller

--- 10-jobster_api_extra_project/jobs-api/routes/jobs.js
+++ 10-jobster_api_extra_project/jobs-api/routes/jobs.js
@@ -8,11 +8,14 @@ const {
   createJob,
   updateJob,
   deleteJob,
+  showStats,
 } = require('../controllers/jobs');
 
 const router = express.Router();
 
 router.route('/').get(getAllJobs).post(testUser, createJob);
+// Stats
+router.route('/stats').get(showStats);
 router
   .route('/:id')
   .get(getJob)
