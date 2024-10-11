96ad69c6d25c9b6a306d0c21d7d12cbac02daa7f
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-11 00:30:59 -0400

    Additional Checks

--- 11-file_image_upload/file-upload/controllers/uploadsController.js
+++ 11-file_image_upload/file-upload/controllers/uploadsController.js
@@ -1,9 +1,27 @@
 const path = require('path');
 const { StatusCodes } = require('http-status-codes');
+// Errors
+const CustomError = require('../errors');
 
 // Upload image
 const uploadProductImage = async (req, res) => {
+  //   console.log(req.files);
+  // Check if file exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No file Uploaded');
+  }
   const productImage = req.files.image;
+  // Check format
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image');
+  }
+  // Check Size (KB)
+  // const maxSize = 1000;
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError('Please upload image smaller 1KB');
+  }
+
   // Return the absolute path
   const imagePath = path.join(
     __dirname,
