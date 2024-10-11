8f69b570b7e0c2f94603581be467ce68394a146d
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-11 18:35:54 -0400

    Send Emails With Ethereal

--- 12-send_emails/send-email/controllers/sendEmail.js
+++ 12-send_emails/send-email/controllers/sendEmail.js
@@ -1,5 +1,27 @@
+const nodemailer = require('nodemailer');
+
 const sendEmail = async (req, res) => {
-  res.send('Send email func');
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false, // true for port 465, false for other ports
+    auth: {
+      user: 'rubye.schmidt@ethereal.email',
+      pass: 'x89dq3sPwXy6JWuMdS',
+    },
+  });
+
+  // Send email with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Adriano Ayala" <adriano.ayala@gmail.com>', // Sender address
+    to: 'adriano@example.com', // List or receives
+    // to: 'adriano@example.com, adriano2@example.com', // List or receives
+    subject: 'Hello', // Subject line
+    text: 'Hello world?', // plain text body
+    html: '<b>Hello world?</b>', // html body
+  });
+
+  res.json(info);
 };
 
 module.exports = sendEmail;
