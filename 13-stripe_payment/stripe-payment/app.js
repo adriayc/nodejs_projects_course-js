6a69f9979b4856a54f3d355bcc83c7ac94f0607d
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-12 23:13:45 -0400

    Setup

---
+++ 13-stripe_payment/stripe-payment/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+require('express-async-errors');
+// Middlewares
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+const app = express();
+
+// Call middleware
+app.use(express.json());
+app.use(express.static('./public'));
+
+// Call custom middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
