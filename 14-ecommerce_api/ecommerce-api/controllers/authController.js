3c2f4a4bdf8169a0b1ce8c662bc8224afce92e9a
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-14 18:25:29 -0400

    Register - Initial Setup

--- 14-ecommerce_api/ecommerce-api/controllers/authController.js
+++ 14-ecommerce_api/ecommerce-api/controllers/authController.js
@@ -1,5 +1,12 @@
+const { StatusCodes } = require('http-status-codes');
+// Models
+const User = require('../models/User');
+// Errors
+const CustomAPIError = require('../errors');
+
 const register = async (req, res) => {
-  res.send('Register user');
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
