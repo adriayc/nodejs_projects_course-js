079519b9bc7b3b731003d695963ee3defef76e6b
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-14 18:26:16 -0400

    User Roles

--- 14-ecommerce_api/ecommerce-api/controllers/authController.js
+++ 14-ecommerce_api/ecommerce-api/controllers/authController.js
@@ -5,14 +5,18 @@ const User = require('../models/User');
 const CustomAPIError = require('../errors');
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, name, password } = req.body;
 
   const emailAlreadyExist = await User.findOne({ email });
   if (emailAlreadyExist) {
     throw new CustomAPIError.BadRequestError('Email already exists');
   }
 
-  const user = await User.create(req.body);
+  // First registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
+
+  const user = await User.create({ name, email, password, role });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
