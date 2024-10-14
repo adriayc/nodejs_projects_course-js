f066f6feca484d1afc91ff72a7f2910ebeb592b1
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-14 17:04:03 -0400

    Complete Payment

--- 13-stripe_payment/stripe-payment/controllers/stripeController.js
+++ 13-stripe_payment/stripe-payment/controllers/stripeController.js
@@ -1,6 +1,21 @@
-const stripe = async (req, res) => {
-  console.log(req.body);
-  res.send('Stripe route');
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+const stripeController = async (req, res) => {
+  // console.log(req.body);
+  const { purchase, total_amount, shipping_fee } = req.body;
+
+  const calculateOrderAmount = () => {
+    return total_amount + shipping_fee;
+  };
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(),
+    currency: 'usd',
+  });
+  // console.log(paymentIntent);
+
+  res.json({ clientSecret: paymentIntent.client_secret });
 };
 
-module.exports = stripe;
+module.exports = stripeController;
