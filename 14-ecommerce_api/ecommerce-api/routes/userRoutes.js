dadd2265bb26a2e1e8592a2af85820cfdb07c21b
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-16 18:44:58 -0400

    Authorize Permissions - Setup

--- 14-ecommerce_api/ecommerce-api/routes/userRoutes.js
+++ 14-ecommerce_api/ecommerce-api/routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 // Middlewares
-const { authenticateUser } = require('../middleware/authentication');
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
 // Controller
 const {
   getAllUsers,
@@ -12,7 +15,7 @@ const {
 
 const router = express.Router();
 
-router.route('/').get(authenticateUser, getAllUsers);
+router.route('/').get(authenticateUser, authorizePermissions, getAllUsers);
 router.route('/showMe').get(showCurrentUser);
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
