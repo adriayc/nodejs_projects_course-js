660245486c1bf091865e803668b6aa0fa6249e65
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-17 15:21:10 -0400

    Auth Complete

--- 14-ecommerce_api/ecommerce-api/utils/checkPermissions.js
+++ 14-ecommerce_api/ecommerce-api/utils/checkPermissions.js
@@ -4,8 +4,12 @@ const checkPermissions = (requestUser, resourceUserId) => {
   //   console.log(requestUser);
   //   console.log(resourceUserId);
   //   console.log(typeof resourceUserId);
-  if (requestUser.role === 'admin') return;
-  if (requestUser.userId === resourceUserId.toString()) return;
+  //   if (requestUser.role === 'admin') return;
+  if (
+    requestUser.role === 'admin' ||
+    requestUser.userId === resourceUserId.toString()
+  )
+    return;
   throw new CustomError.UnAuthorizedError('No authorized to access this route');
 };
 
