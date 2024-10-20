58dd7cd73277cb91286f85a3126dbf6fbea58d9a
Adriano Ayala <adriano.ayc@gmail.com>
2024-10-19 22:01:07 -0400

    Create Order - Check Product

--- 14-ecommerce_api/ecommerce-api/controllers/orderController.js
+++ 14-ecommerce_api/ecommerce-api/controllers/orderController.js
@@ -1,4 +1,50 @@
+const { StatusCodes } = require('http-status-codes');
+// Models
+const Order = require('../models/Order');
+const Product = require('../models/Product');
+// Errors
+const CustomError = require('../errors');
+// Utils
+const { checkPermissions } = require('../utils');
+
 const createOrder = async (req, res) => {
+  const { tax, shippingFee, items: cartItems } = req.body;
+
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomError.BadRequestError('No cart items provided');
+  }
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError(
+      'Please provide tax and shipping fee'
+    );
+  }
+
+  let orderItems = [];
+  let subtotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(`No product with id ${item.product}`);
+    }
+    const { _id, name, price, image } = dbProduct;
+    // console.log(name, price, image);
+
+    const singleOrderItem = {
+      name,
+      image,
+      price,
+      amount: item.amount,
+      product: _id,
+    };
+    // Add item to order
+    orderItems = [...orderItems, singleOrderItem];
+    // Calculate subtotal
+    subtotal += item.amount * price;
+  }
+  console.log(orderItems);
+  console.log(subtotal);
+
   res.send('Create order');
 };
 
